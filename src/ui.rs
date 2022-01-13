184b7c3c0a2c5e01b31491bd7df9fd652bcbee3d
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-01-13 22:52:15 +0000

    Refactor ui

--- src/ui.rs
+++ src/ui.rs
@@ -5,36 +5,39 @@ Basically the HTML/CSS of the program
 use crate::App;
 use tui::{
     backend::Backend,
-    layout::{Constraint, Corner, Direction, Layout},
+    layout::{Constraint, Corner, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
     widgets::{Block, Borders, List, ListItem},
     Frame,
 };
 
-
 pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
-    // Create two chunks with equal horizontal screen space
+    // Split screen horizontally into three chunks
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([
                 Constraint::Percentage(30),
                 Constraint::Percentage(50),
                 Constraint::Percentage(20),
-            ].as_ref(),
-        )
+        ]
+        .as_ref())
         .split(f.size());
 
-    let ver_chunks = Layout::default()
-        .constraints(
-            [
-                Constraint::Percentage(50),
-                Constraint::Percentage(50),
-            ]
-            .as_ref(),
-        )
+    draw_left_section(f, app, chunks[0]);
+    draw_central_timer(f, app, chunks[1]);
+    draw_keybind_help(f, app, chunks[2]);
+}
+
+fn draw_left_section<B: Backend>(f: &mut Frame<B>, app: &mut App, main_chunk: Rect) {
+    let chunks = Layout::default()
+        .constraints([
+            Constraint::Percentage(50), 
+            Constraint::Percentage(50),
+        ]
+        .as_ref())
         .direction(Direction::Vertical)
-        .split(chunks[0]);
+        .split(main_chunk);
     // Iterate through all elements in the `items` app and append some debug text to it.
     let items: Vec<ListItem> = app
         .items
@@ -63,15 +66,19 @@ pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .highlight_symbol(">> ");
 
     // We can now render the item list
-    f.render_stateful_widget(items, ver_chunks[1], &mut app.items.state);
+    f.render_stateful_widget(items, chunks[1], &mut app.items.state);
+}
 
+fn draw_central_timer<B: Backend>(f: &mut Frame<B>, app: &mut App, main_chunk: Rect) {
     // This is the central timer section
     let mut centeral_time = app.time.to_string();
     match centeral_time.len() {
-        0 => {centeral_time = "0.00".to_owned()},
-        1 => {centeral_time = "0.0".to_owned() + &centeral_time},
-        2 => {centeral_time = "0.".to_owned() + &centeral_time},
-        _ => {centeral_time.insert(app.time.to_string().len()-2, '.');}
+        0 => centeral_time = "0.00".to_owned(),
+        1 => centeral_time = "0.0".to_owned() + &centeral_time,
+        2 => centeral_time = "0.".to_owned() + &centeral_time,
+        _ => {
+            centeral_time.insert(app.time.to_string().len() - 2, '.');
+        }
     }
     let text = vec![
         Spans::from(Span::styled(
@@ -89,9 +96,11 @@ pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         vertical: 10,
         horizontal: 30,
     };
-    chunks[1].inner(&timer_margin);
-    f.render_widget(time_text, chunks[1].inner(&timer_margin));
+    main_chunk.inner(&timer_margin);
+    f.render_widget(time_text, main_chunk.inner(&timer_margin));
+}
 
+fn draw_keybind_help<B: Backend>(f: &mut Frame<B>, app: &mut App, main_chunk: Rect) {
     let keybinds: Vec<ListItem> = app
         .keybinds
         .iter()
@@ -119,5 +128,5 @@ pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let events_list = List::new(keybinds)
         .block(Block::default().borders(Borders::ALL).title("Keybinds"))
         .start_corner(Corner::BottomLeft);
-    f.render_widget(events_list, chunks[2]);
+    f.render_widget(events_list, main_chunk);
 }
