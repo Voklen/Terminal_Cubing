c377232d53091e3f74062f427aad8b9d6d0c175c
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-01-13 22:20:57 +0000

    Moved ui to seperate file

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,11 @@ use std::{
 };
 use tui::{
     backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Corner, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::{Block, Borders, List, ListItem, ListState},
-    Frame, Terminal,
+    widgets::ListState,
+    Terminal,
 };
+
+mod ui;
 
 struct StatefulList<T> {
     state: ListState,
@@ -72,7 +71,7 @@ impl<T> StatefulList<T> {
 }
 
 //This struct holds the current state of the app.
-struct App<'a> {
+pub struct App<'a> {
     items: StatefulList<(&'a str, usize)>,
     keybinds: [(&'a str, &'a str); 20],
     time: i32,
@@ -195,7 +194,7 @@ fn run_app<B: Backend>(
     let mut last_tick = Instant::now();
     let mut key_pressed_in_tick = false;
     loop {
-        terminal.draw(|f| ui(f, &mut app))?;
+        terminal.draw(|f| ui::draw(f, &mut app))?;
 
         let timeout = tick_rate
             .checked_sub(last_tick.elapsed())
@@ -220,106 +219,4 @@ fn run_app<B: Backend>(
             key_pressed_in_tick = false;
         }
     }
-}
-
-/*
-Basically the HTML/CSS of the program
-*/
-fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
-    // Create two chunks with equal horizontal screen space
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-                Constraint::Percentage(30),
-                Constraint::Percentage(50),
-                Constraint::Percentage(20),
-            ].as_ref(),
-        )
-        .split(f.size());
-
-    // Iterate through all elements in the `items` app and append some debug text to it.
-    let items: Vec<ListItem> = app
-        .items
-        .items
-        .iter()
-        .map(|i| {
-            let mut lines = vec![Spans::from(i.0)];
-            for _ in 0..i.1 {
-                lines.push(Spans::from(Span::styled(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                    Style::default().add_modifier(Modifier::ITALIC),
-                )));
-            }
-            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
-        })
-        .collect();
-
-    // Create a List from all list items and highlight the currently selected one
-    let items = List::new(items)
-        .block(Block::default().borders(Borders::ALL).title("List"))
-        .highlight_style(
-            Style::default()
-                .bg(Color::LightGreen)
-                .add_modifier(Modifier::BOLD),
-        )
-        .highlight_symbol(">> ");
-
-    // We can now render the item list
-    f.render_stateful_widget(items, chunks[0], &mut app.items.state);
-
-    // This is the central timer section
-    let mut centeral_time = app.time.to_string();
-    match centeral_time.len() {
-        0 => {centeral_time = "0.00".to_owned()},
-        1 => {centeral_time = "0.0".to_owned() + &centeral_time},
-        2 => {centeral_time = "0.".to_owned() + &centeral_time},
-        _ => {centeral_time.insert(app.time.to_string().len()-2, '.');}
-    }
-    let text = vec![
-        Spans::from(Span::styled(
-            centeral_time,
-            Style::default().add_modifier(Modifier::ITALIC),
-        )),
-        Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
-    ];
-    let time_text = tui::widgets::Paragraph::new(text)
-        .block(Block::default().borders(Borders::ALL))
-        .style(Style::default().fg(Color::White).bg(Color::Black))
-        .alignment(tui::layout::Alignment::Center)
-        .wrap(tui::widgets::Wrap { trim: true });
-    let timer_margin = tui::layout::Margin {
-        vertical: 10,
-        horizontal: 30,
-    };
-    chunks[1].inner(&timer_margin);
-    f.render_widget(time_text, chunks[1].inner(&timer_margin));
-
-    let keybinds: Vec<ListItem> = app
-        .keybinds
-        .iter()
-        .rev()
-        .map(|&(event, level)| {
-            // Colorcode the level depending on its type
-            let s = match level {
-                "CRITICAL" => Style::default().fg(Color::Red),
-                "ERROR" => Style::default().fg(Color::Magenta),
-                "WARNING" => Style::default().fg(Color::Yellow),
-                "INFO" => Style::default().fg(Color::Blue),
-                _ => Style::default(),
-            };
-            // Add a example datetime and apply proper spacing between them
-            let header = Spans::from(vec![
-                Span::styled(format!("{:<9}", level), s),
-                Span::raw(" "),
-                Span::styled(event, Style::default().add_modifier(Modifier::ITALIC)),
-            ]);
-            // Add the line to list
-            ListItem::new(header)
-        })
-        .collect();
-    // Border formating
-    let events_list = List::new(keybinds)
-        .block(Block::default().borders(Borders::ALL).title("Keybinds"))
-        .start_corner(Corner::BottomLeft);
-    f.render_widget(events_list, chunks[2]);
 }
